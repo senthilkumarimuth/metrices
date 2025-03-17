633822929ea87b36927f5fb1c32801554d9081d5
senthilkumarimuth <senthilkumarimuthu@gmail.com>
2025-03-17 22:53:47 +0530

    Auto commit Mon Mar 17 22:53:47 2025

--- src/scheduler_daily.py
+++ src/scheduler_daily.py
@@ -1,9 +1,18 @@
 import subprocess
 import time
 from run_all_reports import main as all_reports_main
+import os
+from custom_dirs import RootDirectory
 
 def git_workflow():
     try:
+        print(f"Current directory: {os.getcwd()}")
+        # Set the repo path (replace with your actual path)
+        repo_path = RootDirectory.path
+        os.chdir(repo_path)
+        print(f"Switched to: {os.getcwd()}")
+        # check git is accessible
+        subprocess.run(["git", "--version"], check=True)
         # Add files
         subprocess.run(["git", "add", "."], check=True)
         # Commit
