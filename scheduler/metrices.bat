a50cb2e68cee133529c26c094d5a9993acd78c8c
Suganthi Marimuthu <suganthbanu135@gmail.com>
2025-07-08 13:09:50 +0530

    Auto commit Tue Jul  8 13:09:49 2025

---
+++ scheduler/metrices.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\Senthilkumar M\PycharmProjects\metrices\.venv\Scripts\python.exe" "C:\Users\Senthilkumar M\PycharmProjects\metrices\src\scheduler_daily.py" >> "C:\logs\metrices_log.txt" 2>&1
